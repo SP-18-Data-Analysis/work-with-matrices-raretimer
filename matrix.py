3bd20b9904fd59b31fcf21446db6d85456b8fb94
raretimer <31965788+raretimer@users.noreply.github.com>
2018-01-25 13:19:46 -0600

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -6,6 +6,10 @@ m = numpi.array([[3,1,4],[6,3,9],[3,3,3],[6,7,9],[7,5,2]])
 
 #print out the array
 print (m)
+#or using your hint
+
+for column in m:
+    print(column)
 
 #print out subsequent columns
 print (m[:,0])
@@ -14,7 +18,4 @@ print (m[:,1])
 
 print (m[:,2])
 
-#or using your hint
 
-for column in m:
-    print(column)
