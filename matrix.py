07c038d4c4f8ada7ea4dd066bb7b3e1540750008
raretimer <31965788+raretimer@users.noreply.github.com>
2018-01-25 13:03:39 -0600

    Create matrix.py

---
+++ matrix.py
@@ -0,0 +1,17 @@
+#import the needed operations
+import numpy as numpi
+
+#create array and fill with numbers
+m = numpi.array([[3,1,4],[6,3,9],[3,3,3],[6,7,9],[7,5,2]])
+
+#print out the array
+print (m)
+
+#print out subsequent columns
+print (m[:,0])
+
+print (m[:,1])
+
+print (m[:,2])
+
+
